ba0d8e4110e64a15165532946dfa7b99740b83bd
gpassucc <34295337+gpassucc@users.noreply.github.com>
2018-06-04 10:45:26 -0400

    Update data_gather.m
    Updated comments, removed deprecated sections of code.

--- data_gather.m
+++ data_gather.m
@@ -1,4 +1,4 @@
-function [datas,pcell,collapse_times] = data_gather(xyzs_id,xyzs_id_columns,mu)
+function [datas,pcell,collapse_times,mins] = data_gather(xyzs_id,xyzs_id_columns,mu)
 %{
 This function generates a matrix of speeds, rotational diffusion
 coefficients and run times for trajectories gathered through the ACTIVE
@@ -15,6 +15,46 @@ number of columns of your matrix, where the columns should be x position, y
 position, frame index and cell index, respectively. 
 mu: estimated from the mean-squared displacement (MSD) where alpha is the slope
 of the loglog plot of MSD and timescale, where mu = 3 - alpha. 
+
+OUTPUT: 
+datas: matrix of cell trajectory data with columns run length, speed,
+rotational diffusion and orientation angle
+pcell: Matlab cell structure containing displacement probability
+information
+collapse_times: Frames at which p_cell is calculated
+min: frame indicies where the Canny edge detection algorithm identified a
+tumble
+
+Note: If you don't have experimental images that you will be generating
+trajectory data with, you can create a very simple Brownian particle
+simulation in Matlab. Example code: 
+
+dt = 1;
+v0 = 1;
+a = 1;
+ts = 100;
+np = 100;
+dtmax = ts-1;
+posvec = cell(np,1);
+for i = 1:np
+    rng('shuffle')
+    %disp(num2str(i))
+    xpos = 0;
+    ypos = 0;
+    theta = rand(1)*2*pi;
+    thetaRan = a * (randn(1,ts));
+    posvec{i} = zeros(ts,3);
+    for t=1:ts
+        dtheta = thetaRan(t);
+        theta = theta + dtheta*sqrt(dt);
+        xpos = xpos + v0*cos(theta)*dt;
+        ypos = ypos + v0*sin(theta)*dt;
+        theta = mod(theta,2*pi);
+        posvec{i}(t,1) = xpos;
+        posvec{i}(t,2) = ypos;
+        posvec{i}(t,3) = theta;
+    end
+end
 %}
 frameindx = 3;
 cellindx = 4;
@@ -38,7 +78,7 @@ for i=1:ncells
     ypos(i,cell_tracks(boolcell,frameindx)) = cell_tracks(boolcell,2);    
 end
 
-%to account for small gaps in cell tracking, we reconstruct
+% To account for small gaps in cell tracking, we reconstruct
 %trajectories if the cell has not been missing for more than F frames
 %(where F=5 is default) 
 F = 5;
@@ -70,6 +110,7 @@ for i = 1:ncells
 end
 
 % Calculate pcell and collapse_times for prt_collapse.m
+dtt=1.0;
 collapse_times = [round(ts/5)/dtt round(2*ts/5)/dtt round(3*ts/5)/dtt round(4*ts/5)/dtt];
 pcell = cell(length(collapse_times),1);
 for ii = 1:length(collapse_times)
@@ -88,7 +129,7 @@ for ii = 1:length(collapse_times)
     pcell{ii} = p;
 end
 
-%we impose a limit cutoff=20 of how few frames a cell may be present in
+% We impose a limit cutoff=20 of how few frames a cell may be present in
 %consecutively to be included in our analysis in order to increase accuracy
 cutoff = 20;
 posx = cellfun(@(x) plateau(x.'), xcell,'UniformOutput',0);
@@ -113,14 +154,14 @@ for i = 1:length(posx)
     end
 end
 
-%we utilize Otsu's method (from otsu.m) to 
+% We utilize Otsu's method (from otsu.m) to calculate the Canny thresholds
 ts = zeros(length(dtheta),1);
 for i = 1:length(dtheta)
     try
         d = dtheta{i};
         d = diff(dtheta{i}+pi);
         d = min(2*pi-abs(d),abs(d)); 
-        [s,u,thresh] = otsu(d,0);
+        [thresh] = otsu(d,0);
         ts(i) = thresh/max(d);
     end
 end
@@ -139,72 +180,10 @@ for i = 1:length(dtheta)
         %runs = [runs d];
     end
 end
+mins = cellfun(@find,mins,'UniformOutput',0);
 t0 = mean(runs)/(mu-1);
 disp('done')
-
-cutoff = 20;
-% plateau.m finds the longest continuous portion of a cell track, since we
-% don't want to include areas where the cell doesn't exist for long periods
-% of time
-% calculate positions and cell orientation from individual trajectories
-posx = cellfun(@(x) plateau(x.'), xcell,'UniformOutput',0);
-posy = cellfun(@(x) plateau(x.'), ycell,'UniformOutput',0);
-ind = cellfun(@(x)iscell(x),posx);
-posx = posx(find(1-ind),1);
-posy = posy(find(1-ind),1);
-ind = cellfun(@(x) length(x),posx);
-ind = find(ind > 10);
-posx = posx(ind,1);
-posy = posy(ind,1);
-vx = cellfun(@diff,posx,'UniformOutput',0);
-vy = cellfun(@diff,posy,'UniformOutput',0);
-dtheta = cell(length(posx),1);
-vv = cell(length(posx),1);
-for i = 1:length(posx)
-    if length(posx{i}) > cutoff
-        vv{i} = sqrt(vx{i}(:,1).^2 + vy{i}(:,1).^2);
-        dx = diff(posx{i}(:,1));
-        dy = diff(posy{i}(:,1));
-        dtheta{i} = atan2(dy,dx);
-    end
-end
-dtheta = cellfun(@(x) x+pi,dtheta,'UniformOutput',0);
-dtheta = cellfun(@(x) diff(x),dtheta,'UniformOutput',0);
-dtheta = cellfun(@(x) min(2*pi-abs(x),abs(x)),dtheta,'UniformOutput',0);
-
-
-%n = 78; %78
-mins = cell(length(dtheta),1);
-for n = 1:length(dtheta)
-    t = 4*(posx{n}(1:end-2,2));
-    ma = 3;
-    dT = conv2(dtheta{n},ones(ma,1)/ma,'valid');
-    v = conv2(vv{n}(1:end-1),ones(ma,1)/ma,'valid');
-    [xmax,MaxIdx,xmin,imin] = extrema(dT);
-    [xmaxv,imaxv,xminv,MinIdx] = extrema(v);
-    idx = [];
-    for i = 1:length(MaxIdx)
-        B = [MaxIdx(i)-1 MaxIdx(i) MaxIdx(i)+1];
-        lia = ismember(MinIdx(:,1),B.');
-        if sum(sum(lia)) == 0
-            MaxIdx(i) = 0;
-        elseif sum(sum(lia)) > 0
-            f = find(lia);
-            MinIdx(f(1),2) = 1;
-            idx = [idx.' [MaxIdx(i) MinIdx(f(1),1)].'].';
-        end
-    end
-    if isempty(idx) == 1
-        continue
-    end
-    idx = sort(idx);
-    idx = idx(2:end-1,:);
-    idx(:,3) = abs(min(dT(idx(:,1)-1)-dT(idx(:,1)),dT(idx(:,1)+1)-dT(idx(:,1)))./dT(idx(:,1)));
-    idx(:,4) = max(v(idx(:,2)-1)-v(idx(:,2)),v(idx(:,2)+1)-v(idx(:,2)))./v(idx(:,2));
-    MaxIdx = idx(idx(:,3) > 0 & idx(:,4) > 0 & dT(idx(:,1)) - v(idx(:,2)) > 0,1) + 3;
-    mins{n} = MaxIdx;
-end
-
+ 
 dr = dr(~cellfun('isempty',dr));
 dtheta = dtheta(~cellfun('isempty',dtheta));
 dtheta1 = cellfun(@(x) x+pi,dtheta,'UniformOutput',0);
@@ -247,14 +226,16 @@ for i = 1:length(mins)
         end
         phis = [phis.' mod(diff(phi),2*pi).'].';
         T = dtheta{i}(ind);
-        datasT = [datasT.' [d*4 v.' a T(2:end) ].'].';
+        datasT = [datasT.' [d v.' a T(2:end) ].'].';
     end
 end
 ind_good = datasT(:,2) ~= 0;
-datas = [datas.' datasT(ind_good,:).'].';
+%datas = [datas.' datasT(ind_good,:).'].';
+datas = datasT(ind_good,:);
 
 % Calculate pcell and collapse_times for prt_collapse.m
-collapse_times = [round(ts/5)/dtt round(2*ts/5)/dtt round(3*ts/5)/dtt round(4*ts/5)/dtt];
+Ts = max(xyzs_id(:,3));
+collapse_times = [round(Ts/5)/dtt round(2*Ts/5)/dtt round(3*Ts/5)/dtt round(4*Ts/5)/dtt];
 pcell = cell(length(collapse_times),1);
 for ii = 1:length(collapse_times)
     nsampled = 0;
@@ -262,7 +243,7 @@ for ii = 1:length(collapse_times)
     p = zeros(nframes-dt-2,ncells);
     for i = 1:ncells
         for j = find(xcell{i}) 
-            if j+dt < ts && xcell{i}(j) ~= 0 && xcell{i}(j+dt) ~= 0
+            if j+dt < Ts && xcell{i}(j) ~= 0 && xcell{i}(j+dt) ~= 0
                 dr = sqrt((xcell{i}(j+dt) - xcell{i}(j))^2 + (ycell{i}(j+dt) - ycell{i}(j))^2);
                 p(j,i) = dr;
                 nsampled = nsampled + 1;
@@ -281,7 +262,7 @@ pp = pp(pp~=0);
 [CDF,X] = ksdensity(pp,'function','cdf','support','positive');
 pH = diff(CDF)./mean(diff(X));
 xH = X(1:end-1);
-collapse_times(1) = tPRT;
+tPRT = collapse_times;
 
 posx = cellfun(@(x) plateau(x.'), xcell,'UniformOutput',0);
 posy = cellfun(@(x) plateau(x.'), ycell,'UniformOutput',0);
@@ -366,5 +347,4 @@ for n = 1:ntrials
     m = cell2mat(m).';
     msd(n,:) = log10(m);
 end
-save(filename,variables)
 save('traj_info.mat','msd','acorr','xH','pH','tPRT')
